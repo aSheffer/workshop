b4d2d13e777fa39988c879c5c63442a159fc754b
aSheffer <blablaasi@gmail.com>
2021-03-01 21:26:20 +0200

    Create Aclass.py

---
+++ Aclass.py
@@ -0,0 +1,18 @@
+class A():
+	def __init__(self, a='a'):
+		self.a = a
+
+	def get_a(self):
+		return self.a
+
+	def set_a(self, a):
+		self.a=a
+
+	def print_(self):
+		print("a =", self.a)
+
+	def print_branch(self):
+		print("On branch SDN")
+		
+	def print_branch_auth(self):
+		print("On branch auth")
